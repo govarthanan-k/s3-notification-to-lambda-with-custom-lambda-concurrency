d455003649cb7217c419334625a68d3a7c40c492
Govarthanan K <govarthanank@Govarthanans-MacBook-Pro.local>
2022-09-28 16:47:16 +0530

    Initial commit

--- lib/custom-lambda-with-reserved-concurrency-stack.ts
+++ lib/custom-lambda-with-reserved-concurrency-stack.ts
@@ -1,16 +1,15 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { RemovalPolicy } from "aws-cdk-lib";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
 
 export class CustomLambdaWithReservedConcurrencyStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CustomLambdaWithReservedConcurrencyQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new Bucket(this, "id", {
+      bucketName: "gova-test-bucket-1",
+      publicReadAccess: false,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
   }
 }
