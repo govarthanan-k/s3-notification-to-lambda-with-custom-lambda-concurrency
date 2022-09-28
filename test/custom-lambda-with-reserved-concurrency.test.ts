f2feb4c8c54ec1303a9d8ca61affed85381045e9
Govarthanan K <govarthanank@Govarthanans-MacBook-Pro.local>
2022-09-28 16:09:20 +0530

    Initial commit

---
+++ test/custom-lambda-with-reserved-concurrency.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CustomLambdaWithReservedConcurrency from '../lib/custom-lambda-with-reserved-concurrency-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/custom-lambda-with-reserved-concurrency-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CustomLambdaWithReservedConcurrency.CustomLambdaWithReservedConcurrencyStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
